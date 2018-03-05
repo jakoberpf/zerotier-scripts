c5ef2bea1cb6ff7b7026986963a5bd0d7d532415
Matt Elliott <shamsway@users.noreply.github.com>
2018-03-05 14:42:09 -0500

    Fancier logic for ZeroTier IP checking

--- zerotier-installer.sh
+++ zerotier-installer.sh
@@ -94,13 +94,25 @@ zerotier-cli -j info > zt-info
 ZTADDRESS=$(cat zt-info|jq ".address"| tr -d '"')
 if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "My ZeroTier address is $ZTADDRESS" ; fi
 curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "config":{ "authorized": true } }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
-sleep 20
 curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "name":"'$(hostname)'" }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
-sleep 10
 #curl -H "Authorization: bearer $ZTAPI" https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS > zt-member
 #ZTIP=$(cat zt-member | jq ".config.ipAssignments" | tr -d '[]" \n')
-ZTIP=$(ip addr show zt0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "My ZeroTier IP is $ZTIP. VM Customization Complete." ; fi
-echo ZeroTier IP: $ZTIP
+
+COUNTER=9
+
+while [  $COUNTER -gt 0 || -z "$ZTIP" ]; do
+  ZTIP=$(ip addr show zt0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+  if [ -n "$ZTIP" ]; then
+    if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "My ZeroTier IP is $ZTIP. VM customization Complete." ; fi
+    echo ZeroTier IP: $ZTIP
+  fi
+  sleep 10
+  let COUNTER-=1
+done
+
+if [ -z "$ZTIP" ]; then
+  if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "Could not determine ZeroTier IP. VM customization complete." ; fi
+fi
+
 rm zt-info
 rm zt-member
