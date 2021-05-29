e823b0605c4d7bb0922ff70278e04f159c403106
Jakob <Erpf>
2021-05-29 12:20:43 +0200

    reduce installer

--- zerotier-installer.sh
+++ zerotier-installer.sh
@@ -88,31 +88,3 @@ $SUDO apt-key add /tmp/zt-gpg-key
 $SUDO apt-get update
 $SUDO apt-get install -y zerotier-one
 $SUDO rm -f /tmp/zt-gpg-key
-
-zerotier-cli join $ZTNETWORK 
-zerotier-cli -j info > zt-info
-ZTADDRESS=$(cat zt-info|jq ".address"| tr -d '"')
-if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "My ZeroTier address is $ZTADDRESS" ; fi
-curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "config":{ "authorized": true } }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
-curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "name":"'$(hostname)'" }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
-#curl -H "Authorization: bearer $ZTAPI" https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS > zt-member
-#ZTIP=$(cat zt-member | jq ".config.ipAssignments" | tr -d '[]" \n')
-
-COUNTER=9
-
-while [  $COUNTER -gt 0 ] && [ -z "$ZTIP" ]; do
-  ZTIP=$(ip addr show zt0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-  if [ -n "$ZTIP" ]; then
-    if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "My ZeroTier IP is $ZTIP. VM customization Complete." ; fi
-    echo ZeroTier IP: $ZTIP
-  fi
-  sleep 10
-  let COUNTER-=1
-done
-
-if [ -z "$ZTIP" ]; then
-  if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "Could not determine ZeroTier IP. VM customization complete." ; fi
-fi
-
-rm zt-info
-rm zt-member
