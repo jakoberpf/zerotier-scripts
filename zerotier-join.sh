01866d310a3d8059571f74197a254395d67e0611
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-05-31 13:24:04 +0200

    fix join script

--- zerotier-join.sh
+++ zerotier-join.sh
@@ -2,22 +2,24 @@
 
 ICON_EMOJI=:man-lifting-weights:
 HOSTNAME=$(hostname)
-LOCALIP=$(ip addr show ens160 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 if [ -n "$SLACK_WEBHOOK_URL" ]; then
   apt install python-pip -y
   pip install slack-webhook-cli
-  slack -u $HOSTNAME -e $ICON_EMOJI "I'm up! My local ip is $LOCALIP. I'm joining ZeroTier network $ZTNETWORK"
+  slack -u $HOSTNAME -e $ICON_EMOJI "I'm up! I'm joining ZeroTier network $ZTNETWORK"
 fi
 echo Joining Network: $ZTNETWORK
 
 zerotier-cli join $ZTNETWORK 
 zerotier-cli -j info > zt-info
-ZTADDRESS=$(cat zt-info|jq ".address"| tr -d '"')
+
+apt install jq
+ZTADDRESS=$(cat zt-info | jq ".address" | tr -d '"')
+
 if [ -n "$SLACK_WEBHOOK_URL" ]; then slack -u $HOSTNAME -e $ICON_EMOJI "My ZeroTier address is $ZTADDRESS" ; fi
 if [ -n "$ZTIP" ]; then curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "config":{ "ipAssignments":{ "${ZTIP}" } } }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
 curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "config":{ "ip": true } }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
-curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "name":"'$(hostname)'" }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
+curl -H "Authorization: bearer $ZTAPI" -H "Content-Type: application/json" -X POST -d '{ "name":"'$(HOSTNAME)'" }' https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS
 # curl -H "Authorization: bearer $ZTAPI" https://my.zerotier.com/api/network/$ZTNETWORK/member/$ZTADDRESS > zt-member
 # ZTIP=$(cat zt-member | jq ".config.ipAssignments" | tr -d '[]" \n')
 
